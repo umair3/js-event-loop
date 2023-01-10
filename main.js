300d2c63e8a88f6133b9f899d3a3c071a26b7f63
Moon ijaz <moonijaz006@gmail.com>
2023-01-10 17:02:47 +0500

    Add js file

---
+++ main.js
@@ -0,0 +1,85 @@
+
+//Adding number line 
+
+let textarea = document.querySelector('textarea')
+let lineNumbers = document.querySelector('.line-numbers')
+
+textarea.addEventListener('keyup', event => {
+    let numberOfLines = event.target.value.split('\n').length
+
+    lineNumbers.innerHTML = Array(numberOfLines)
+        .fill('<span></span>')
+        .join('')
+})
+textarea.addEventListener('keydown', event => {
+    if (event.key === 'Tab') {
+        let start = textarea.selectionStart
+        let end = textarea.selectionEnd
+
+        textarea.value = textarea.value.substring(0, start) + '\t' + textarea.value.substring(end)
+
+        event.preventDefault()
+    }
+})
+
+// Prototypal Inheritance
+function Stack()
+{
+    this.items = []; //[1, 2, 3, 4]
+}
+
+Stack.prototype.push = function (v) {
+    // add an item
+}
+Stack.prototype.pop = function () {
+    // returns top value and remove it
+    return 0
+}
+Stack.prototype.peek = function () {
+    // read top value
+}
+Stack.prototype.isEmpty = function () {
+    // true if stack is empty
+    // false if stack is not empty
+    if(this.items.length == 0)
+        return true;
+    return false;
+}
+
+
+call_stack = new Stack()
+//call_stack.isEmpty is not a function
+console.log("call stack is empty: " + call_stack.isEmpty()); // Uncaught TypeError: Cannot read properties of undefined (reading 'isEmpty')
+
+// [1], [1, 2, 3]
+
+function Queue()
+{
+    this.items = []; //[1, 2, 3, 4]
+}
+
+Queue.prototype.enqueue = function (v) {
+    // add an item
+}
+Queue.prototype.dequeue = function () {
+    // returns top value and remove it
+    return 0
+}
+Queue.prototype.rear = function () {
+    // read last value
+}
+Queue.prototype.front = function () {
+    // read first value
+}
+Queue.prototype.isEmpty = function () {
+    // true if stack is empty
+    // false if stack is not empty
+    if(this.items.length == 0)
+        return true;
+    return false;
+}
+
+
+callback_queue = new Queue()
+
+console.log("callback_queue is empty: " + callback_queue.isEmpty()); 
