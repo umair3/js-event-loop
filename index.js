afd3d0cb776d908ab853c865ef4b366021596331
Umair Anwar <umair.anwr@gmail.com>
2023-01-21 22:36:46 +0500

    Fix modules path for ds.js."

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {Queue, Stack} from "./ds";
+import {Queue, Stack} from "./modules/ds.js";
 
 
-call_stack = new Stack()
+let call_stack = new Stack();
 console.log("call stack is empty: " + call_stack.isEmpty()); // Uncaught TypeError: Cannot read properties of undefined (reading 'isEmpty')
