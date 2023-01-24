96ed4ca2927351007215bf4f1d659a1dff5db77e
Moon ijaz <moonijaz006@gmail.com>
2023-01-24 16:28:14 +0500

    Add button js

---
+++ modules/buttons.js
@@ -0,0 +1,7 @@
+function makeChange() {
+    document.getElementById("b1").style.display = "flex";
+    document.getElementById("b2").style.display = "flex";
+    document.getElementById("b3").style.display = "flex";
+    document.getElementById("b4").style.display = "flex";
+    document.getElementById("a").style.visibility="hidden";
+}
