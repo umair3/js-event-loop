f1c454596908246157e6afc5aff6c11a49e9805f
Moon ijaz <moonijaz006@gmail.com>
2023-01-28 21:41:14 +0500

    REmove

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import {Queue, Stack} from "./modules/ds.js";
-import { makeChange } from "./modules/buttons.js";
 
 let call_stack = new Stack();
 console.log("call stack is empty: " + call_stack.isEmpty()); // Uncaught TypeError: Cannot read properties of undefined (reading 'isEmpty')
 
-let val = makeChange();
