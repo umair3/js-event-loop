7a310ace28e98f4f3fb2aedd8266a3ad8c9e30c8
Moon ijaz <moonijaz006@gmail.com>
2023-01-28 21:42:07 +0500

    Remove

--- modules/buttons.js
+++
@@ -1,9 +0,0 @@
-function makeChange() {
-    document.getElementById("a").style.visibility="hidden";
-    document.getElementById("b1").style.display = "flex";
-    document.getElementById("b2").style.display = "flex";
-    document.getElementById("b3").style.display = "flex";
-    document.getElementById("b4").style.display = "flex";
-    
-}
-export {makeChange};
