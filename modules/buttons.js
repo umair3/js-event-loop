359d6ac512e662e1e053cde1be85f83192a7dbc6
Moon ijaz <moonijaz006@gmail.com>
2023-01-28 21:30:35 +0500

    Add files

--- modules/buttons.js
+++ modules/buttons.js
@@ -1,9 +1,8 @@
 function makeChange() {
-    document.getElementById("a").style.visibility="hidden";
     document.getElementById("b1").style.display = "flex";
     document.getElementById("b2").style.display = "flex";
     document.getElementById("b3").style.display = "flex";
     document.getElementById("b4").style.display = "flex";
-    
+    document.getElementById("a").style.visibility="hidden";
 }
 export {makeChange};
